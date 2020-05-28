eda3b9b2963e12c1b26c80413cc3ef6b84a0f054
gzstyp <444141300@qq.com>
2020-05-28 20:53:05 +0800

    连接成功后推送未完成的任务

--- src/main/java/com/fwtai/Launch.java
+++ src/main/java/com/fwtai/Launch.java
@@ -1,7 +1,9 @@
 package com.fwtai;
 
+import com.fwtai.websocket.SpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
@@ -10,5 +12,10 @@ public class Launch{
 
     public static void main(String[] args) {
         SpringApplication.run(Launch.class, args);
+    }
+
+    @Bean
+    public SpringUtil springUtil(){
+        return new SpringUtil();
     }
 }
