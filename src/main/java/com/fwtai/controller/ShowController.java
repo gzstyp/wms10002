950b292df9ef3b0663166410d9fb45c677973fb8
gzstyp <444141300@qq.com>
2020-05-30 13:55:38 +0800

    padding:8px 0;-->padding:6px 0;

--- src/main/java/com/fwtai/controller/ShowController.java
+++ src/main/java/com/fwtai/controller/ShowController.java
@@ -4,19 +4,15 @@ import com.fwtai.bean.PageFormData;
 import com.fwtai.service.ShowService;
 import com.fwtai.tool.ToolClient;
 import io.swagger.annotations.Api;
-import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
-import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 
 /**
  * 大屏显示处理
@@ -67,31 +63,5 @@ public class ShowController{
     @GetMapping("getAllFloorMap")
     public void getAllFloorMap(final HttpServletResponse response){
         ToolClient.responseJson(service.getAllFloorMap(),response);
-    }
-
-    @PostMapping("/imageInfo")
-    public final void photo(final HttpServletRequest request,final HttpServletResponse response){
-        try {
-            final PageFormData formData = new PageFormData(request);
-            MultipartHttpServletRequest mhsr = null;
-            try {
-                mhsr =  (MultipartHttpServletRequest) request;
-            } catch (Exception e) {
-                System.out.println("请上传照片1");
-            }
-            if(mhsr == null){
-                System.out.println("未上传照片2");
-            }
-            final DiskFileItemFactory fac = new DiskFileItemFactory();
-            final ServletFileUpload upload = new ServletFileUpload(fac);
-            String originalPath = null;
-            upload.setHeaderEncoding("utf-8");
-            mhsr.setCharacterEncoding("utf-8");
-            final String sys = File.separator;
-            String dirType = sys;
-            ToolClient.responseJson(ToolClient.createJsonSuccess("上传成功"),response);
-        } catch (Exception e) {
-            ToolClient.responseJson(ToolClient.exceptionJson(),response);
-        }
     }
 }
