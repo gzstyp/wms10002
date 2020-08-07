183c9044564f7aa08accf5163eb1a26cee50c195
gzstyp <444141300@qq.com>
2020-08-07 09:25:21 +0800

    海康威视工具类

--- src/main/java/com/fwtai/tool/GetCameraPreviewURL.java
+++ src/main/java/com/fwtai/tool/GetCameraPreviewURL.java
@@ -36,12 +36,12 @@ public class GetCameraPreviewURL {
         /**
          * STEP4：设置参数提交方式
          */
-        String contentType = "application/json";
+        final String contentType = "application/json";
 
         /**
          * STEP5：组装请求参数
          */
-        JSONObject jsonBody = new JSONObject();
+        final JSONObject jsonBody = new JSONObject();
         jsonBody.put("cameraIndexCode", "748d84750e3a4a5bbad3cd4af9ed5101");
         jsonBody.put("streamType", 0);
         jsonBody.put("protocol", "rtsp");
@@ -51,7 +51,7 @@ public class GetCameraPreviewURL {
         /**
          * STEP6：调用接口
          */
-        String result = ArtemisHttpUtil.doPostStringArtemis(path, body, null, null, contentType , null);// post请求application/json类型参数
+        final String result = ArtemisHttpUtil.doPostStringArtemis(path, body, null, null, contentType , null);// post请求application/json类型参数
         return result;
     }
 
